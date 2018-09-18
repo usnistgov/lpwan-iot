989d7e95b02224afd4319f7b42152f4c81a8b526
Lee Badger <lee.badger@nist.gov>
2018-09-18 11:33:12 -0400

    Debugged serial communication with the dataTaker. Now the Fipy communicates both with the laptop and the dataTaker, using two different UARTs. The main.py file now contains detailed instructions on how to set the pins and physical connections between the devices and adapters.

--- datalogger/talk-through-fipy/main.py
+++ datalogger/talk-through-fipy/main.py
@@ -1,5 +1,3 @@
-#IN-PROGRESS code.
-
 # This software was developed at the National Institute of Standards
 # and Technology by employees of the Federal Government in the course
 # of their official duties.  Pursuant to title 17 Section 105 of the
@@ -18,11 +16,42 @@
 # to imply that the entities, materials, or equipment are necessarily
 # the best available for the purpose.
 
-"""Program, to be run on a pycom fipy, to collect data incrementally
-from a dataTaker datalogger over the dataTaker's serial port.
+"""Program, to be run on a pycom fipy, to collect data from a
+dataTaker datalogger over the dataTaker's serial port.
 
-Initialize UART1 for talking with the datataker.
-We are using UART0 for the console.
+In this configuration, the Fipy is connected both to a laptop (via
+UART0) and to the dataTaker device (via UART1).
+
+Important connectivity info:
+
+FT232 USB-to-TTL converter board plugged into the laptop's USB: 
+    the voltage level switch set to 5V VCC
+    VCC connected to Red
+    GND connected to Yellow
+    TXD connected to Green
+    RXD connected to Purple
+
+On the Fipy:
+    Red connected to + power rail on breadboard
+    Yellow connected to - power rail on breadboard
+    Green connected to Fipy Pin 40 (the program port's RX0)
+    Purple connected to Fipy Pin 41 (the program port's TX0)
+
+NulSom INc. Ultra Compact RS232 to TTL Converter with Male DB9:
+    Connected using the DB9 connector, through a Null Modem adapter, to
+        the dataTaker's serial port. The Null Modem adapter is needed.
+    The TTL side of this device is connected using different wires from
+        those described above (but sometimes the same colors)
+    VCC connected to Red
+    GND connected to Black
+    RX connected to White
+    Tx connected to Brown
+
+On the Fipy:
+    Red connected to + power rail on breadboard
+    Black connected to - power rail on breadboard
+    Brown connected to Pin 24 (the Tx of UART1)
+    White connected to Pin 21 (the Rx of UART1)
 """
 
 import sys
@@ -37,7 +66,8 @@ def main():
     print('From my pycom!')
 
     uart = get_configured_uart()
-    time.sleep(0.2)
+    time.sleep(1.0)
+
     jobs_list = get_jobs_list_from_datataker(uart)
     print('the returned jobs: ', jobs_list)
 
@@ -49,12 +79,12 @@ def get_jobs_list_from_datataker(uart):
     """Return a list of jobs currently configured in the datataker."""
     jobs_list = []
     s = uart.write('listd\r\n')
-    time.sleep(0.4)  #NOTE!  may have to add delay for real dataTaker
+    time.sleep(1.0)  #NOTE! static delays may need tuning for dataTaker
     while uart.any():
-        #bytes_value = uart.readline()
-        #line = bytes_value.decode()
-        line = read_a_line(uart)
+        bytes_value = uart.readline()
+        line = bytes_value.decode()
         if line:
+            #print('line:', line)
             #First line of the listd output
             if line.strip() == 'listd':
                 time.sleep(0.2)
@@ -64,7 +94,7 @@ def get_jobs_list_from_datataker(uart):
 
             #Second line of the listd output
             if line_parts[0] == 'Job' and line_parts[1] == 'Sch':
-                time.sleep(02)
+                time.sleep(0.2)
                 continue
 
             #Third line of the listd output
@@ -72,10 +102,15 @@ def get_jobs_list_from_datataker(uart):
                 time.sleep(0.2)
                 continue
 
+            #Last line of the listd output
+            if line.strip() == 'DT80>':
+                time.sleep(0.2)
+                continue
+
             job_path = line_parts[-1]
             jobs_list.append(job_path)
         else:
-            print('read_a_line failed!')
+            print('uart.readline() failed!')
         time.sleep(0.2)
     return(jobs_list)
 
@@ -84,22 +119,21 @@ def get_records_from_datataker(uart):
     """Return a list of (time, temp) tuples, collected from the dataTaker."""
     records = []
     s2 = uart.write('copyd archive=y start=2018-08-29T16:00:00.000\r\n')
-    time.sleep(0.4) #NOTE!  may have to add delay for real dataTaker
+    time.sleep(4.0) #NOTE! may have to tune the static delay for the dataTaker
     while uart.any():
-        #bytes_value = uart.readline()
-        #line = bytes_value.decode()
-        line = read_a_line(uart)
+        bytes_value = uart.readline()
+        line = bytes_value.decode()
         if line:
             line_parts = line.split(',')
-            print('len:', len(line_parts))
-            print('part0:', line_parts[0])
+            #print('len:', len(line_parts))
+            #print('part0:', line_parts[0])
 
             #Filter out the header line.
             if is_time_value(line_parts[0]):
                 rec = tuple([line_parts[0], line_parts[2]])
                 records.append(rec)
         else:
-            print('read_a_line failed!')
+            print('uart.readline() failed!')
         time.sleep(0.4)
     return(records)
 
@@ -138,49 +172,13 @@ def is_time_value(val):
     return(True)
 
 
-def read_a_line(uart):
-    """Read a line from the passed serial port; assume '\n' termination.
-
-    Returns a string.
-    """
-    line = ''
-    while True:
-        if uart.any():
-            one_byte = uart.read(1)
-            line += one_byte.decode()
-            if line.endswith('\n'):
-                return(line)
-        else:
-            time.sleep(0.1)
-
-
 def get_configured_uart():
     """Allocates and initializes a UART object, and returns it."""
-    uart = UART(1, 9600)
-    uart.init(9600, bits=8, parity=None, stop=1)
+    #datataker default settings
+    uart = UART(1, 57600)
+    uart.init(57600, bits=8, parity=None, stop=1)
     return(uart)
 
-
-"""
-# Trivial serial module use to communicate with the dataTaker.
-
-ser = serial.Serial(
-    port = '/dev/cu.usbserial',
-    baudrate=57600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    xonxoff=True
-)
-
-print ser
-
-num = ser.write(b'dir\r\n')
-print 'num is: ', num
-time.sleep(1)
-
-response = ser.read(80)
-"""
 
 if __name__ == '__main__':
     main()
